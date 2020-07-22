345d6dc692de3a0accf044dd042bc1529712ba12
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-07-22 17:49:37 -0400

    practice user input and string concatenation

---
+++ hello_you.py
@@ -0,0 +1,32 @@
+#ask user for name
+
+name = input('What is your name?: ')
+
+#ask user for age
+
+age = input('How old are you?: ')
+
+#ask user for location
+
+location = input('Where are you from?: ')
+
+#ask user for hobby
+
+hobby = input('What is your favorite thing to do?: ')
+
+#create output including vars
+
+sentence = "Your name is {} and you are {} years old. You live in {}, and you love to {}."
+output = sentence.format(name,age,location,hobby)
+
+#print output
+###.format -> automatically changes all variable types to strings
+
+print(output)
+
+print(name)
+print(age)
+print(location)
+print(hobby)
+
+
