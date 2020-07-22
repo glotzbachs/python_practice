762a9b5669b0eae2e86f0bc615a838eba5951289
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-07-22 17:30:09 -0400

    practice strings and string errors

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+"hello"
+
+name = "Sarah"
+
+print(type(name))
+
+message = 'John said, "I will see you later"'
+print(message)
+
+punctiation_trouble = '''Hey, I can say, "There's conjunctions"'''
+
+
+
+hello = "Hello, World!"
+print(hello)
