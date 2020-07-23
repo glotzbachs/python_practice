6e112815212f8c3de57316d5021d6a37f4950d86
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-07-23 17:48:49 -0400

    practice booleans and conditionals

---
+++ if_statements.py
@@ -0,0 +1,23 @@
+num1=100
+num2=100
+
+
+if num1>num2 :
+    print("First number is bigger!")
+elif num1<num2 :
+    print("Second number is bigger!")
+else:
+    print("The numbers are equal!")
+
+
+# if condition1:
+#   code if condition1 true..
+# elif condition2:
+#   code if condition2 true..
+# elif condition3:
+#   code if condition3 true..
+#  .
+#  .
+#  .
+# else :
+#   code..
